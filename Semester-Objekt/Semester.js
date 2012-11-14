764e59bca0c5fd33adb18ce97785afb43e1d2b8b
Chang Fu <fuchang913@gmail.com>
2012-11-14 15:34:28 +0100

    Revert "Revert "Aufgabe""
    This reverts commit 0552b43c1b153ba4245321bec86e8106e38a988a.

--- Semester-Objekt/Semester.js
+++ Semester-Objekt/Semester.js
@@ -3,6 +3,57 @@
 // =============================================================
 
 var mein_semester = {
-  // bilde hier dein Semester ab!
+  student: {
+	name:"Chang Fu",
+	studiegang: "Interface Design",
+	studiesemester: 3,
+	welches: "WS",
+    jahr: "2012/2013",
+	},
+	 
+  veranstaltung:[
+   { 
+	titel: "Sound/Idea",
+    sws: 2,
+    credits:2,
+    workload:90
+    },
+   {
+	title:"Read & Write",
+	sws:2,
+	credits:3,
+    workload:90
+    },
+   {
+	title:"vvvv",
+	sws:2,
+	credits:2,
+    workload:90
+	},
+   {
+	title:"Scriptings",
+	sws:2,
+	credits:3,
+    workload:90
+	},
+   {
+	title:"poetische Apparate",
+	sws:2,
+	credits:3,
+    workload:90
+	}
+   ]
 };
 
+JSON.stringify(mein_semester);
+	// dann kommt:"{"student":{"name":"Chang Fu","studiegang":
+	//"Interface Design","studiesemester":3,"welches":"WS",
+	//"jahr":"2012/2013"},"veranstaltung":
+	//[{"titel":"Sound/Idea","sws":2,"credits":2,"workload":90},
+	//{"title":"Read & Write","sws":2,"credits":3,"workload":90},
+	//{"title":"vvvv","sws":2,"credits":2,"workload":90},
+	//{"title":"Scriptings","sws":2,"credits":3,"workload":90},
+	//{"title":"poetische Apparate","sws":2,"credits":3,"workload":90}]}"
+	
+var jsoned_semester = JSON.stringify (mein_semester);
+JSON.parse(jsoned_semester);
